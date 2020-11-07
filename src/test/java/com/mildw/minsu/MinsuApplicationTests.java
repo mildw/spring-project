a6b18035913d0465629d9f209cce2ae2a8aa2efa
mildw <mildw428@gmail.com>
2020-11-07 17:21:12 +0900

    first commit

---
+++ src/test/java/com/mildw/minsu/MinsuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mildw.minsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
