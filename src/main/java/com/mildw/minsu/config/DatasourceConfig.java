f6cccfe0c392bab1c0cd7f8e1113bdc8bd47a221
김민수 <kms0428@midasit.com>
2020-11-21 15:15:11 +0900

    H2 메모리 DB사용

--- src/main/java/com/mildw/minsu/config/DatasourceConfig.java
+++ src/main/java/com/mildw/minsu/config/DatasourceConfig.java
@@ -1,8 +1,10 @@
 package com.mildw.minsu.config;
 
+import org.h2.tools.Server;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -11,6 +13,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.Properties;
 
 @Configuration
@@ -32,13 +35,20 @@ public class DatasourceConfig {
         return dataSourceBuilder.build();
     }
 
-    private DataSource getDataSourceH2() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:tcp://localhost/~/test");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("");
-        return dataSourceBuilder.build();
+    public DataSource getDataSourceH2() {
+        try {
+            Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9093").start();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("1234");
+        dataSource.setUrl(
+                "jdbc:h2:mem:aicc;INIT=CREATE SCHEMA IF NOT EXISTS aicc;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        return dataSource;
     }
 
     @Bean
